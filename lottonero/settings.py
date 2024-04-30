2e0a1b41e55781be7a04fc887217e7e098434bc7
jjsemaan <jalal.semaan@parabim.com>
2024-04-30 17:46:01 +0000

    Add static root and run colstatic

--- lottonero/settings.py
+++ lottonero/settings.py
@@ -163,6 +163,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
