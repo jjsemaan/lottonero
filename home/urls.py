92191f34560f54750dc77bccac1df40f210e2f97
jjsemaan <jalal.semaan@parabim.com>
2024-05-04 13:02:08 +0000

    Add latest_draw in home/views.py

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import latest_draw
 from .import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('latest-draw/', views.latest_draw, name='latest_draw'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from scraping.models import EuroMillionsResult
 
 # Create your views here.
 
@@ -6,3 +7,12 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def latest_draw(request):
+    latest_result = EuroMillionsResult.objects.latest('id')  # Assuming 'id' or you can also use 'draw_date'
+    context = {
+        'latest_result': latest_result,
+        'formatted_date': latest_result.draw_date,  # Using the draw_date directly from the database
+    }
+    return render(request, 'home/index.html', context)
