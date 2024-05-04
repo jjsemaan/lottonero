acde41de9e806d09c3b23a56ffecf562af8faadf
jjsemaan <jalal.semaan@parabim.com>
2024-05-04 13:39:45 +0000

    Simplify latest_draw view

--- home/views.py
+++ home/views.py
@@ -9,16 +9,9 @@ def index(request):
     return render(request, 'home/index.html')
 
 
-from django.shortcuts import render
-from scraping.models import EuroMillionsResult  # Ensure correct import
-
 def latest_draw(request):
-    try:
-        latest_result = EuroMillionsResult.objects.latest('id')  # Use 'id' if that's a reliable field
-        print("Latest result fetched successfully:", latest_result)  # Debug statement
-    except EuroMillionsResult.DoesNotExist:
-        latest_result = None
-        print("No results found.")  # Debug statement
-    
-    return render(request, 'home/index.html', {'latest_result': latest_result})
-
+    latest_result = EuroMillionsResult.objects.latest('id')  # Assuming 'id' or you can also use 'draw_date'
+    context = {
+        'latest_result': latest_result,
+    }
+    return render(request, 'home/index.html', context)
