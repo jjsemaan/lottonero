29494efc55d483d716c4dbc7fcbe41c08f7261f0
jjsemaan <jalal.semaan@parabim.com>
2024-05-05 18:33:46 +0000

    Add predictions model and trigger button in backoffice

--- predictions/models.py
+++ predictions/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Prediction(models.Model):
+    prediction_date = models.CharField(max_length=10)  # YYYY/MM/DD format
+    pred_ball_1 = models.IntegerField()
+    pred_ball_2 = models.IntegerField()
+    pred_ball_3 = models.IntegerField()
+    pred_ball_4 = models.IntegerField()
+    pred_ball_5 = models.IntegerField()
+    pred_lucky_1 = models.IntegerField()
+    pred_lucky_2 = models.IntegerField()
+
+    def __str__(self):
+        return f"Prediction on {self.prediction_date}"
+
--- predictions/urls.py
+++ predictions/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import train_classifier
 from . import views
 
 urlpatterns = [
     path('predictions/', views.train_classifier, name='predictions'),
     path('backoffice/', views.backoffice, name='backoffice'),
+    path('train/', train_classifier, name='train_classifier'),
 ]
