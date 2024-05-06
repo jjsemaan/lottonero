d5b0692eaf2ed5cab2a89af647054f7ad48a8d4b
jjsemaan <jalal.semaan@parabim.com>
2024-05-06 15:13:55 +0000

    Add heroku to csrf trusted token

--- lottonero/settings.py
+++ lottonero/settings.py
@@ -17,26 +17,22 @@ SECRET_KEY = os.environment.get("SECRET_KEY")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9zm4#j8s%#_+e^p(%yncfp82@n31)fys7fm!5!c6tv&kktngz8'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-jjsemaan-lottonero-jwr574l49n.us1.codeanyapp.com',
-    'lottonero-e7dc9a7038d2.herokuapp.com'
+    'lottonero-e7dc9a7038d2.herokuapp.com',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'https://8000-jjsemaan-lottonero-jwr574l49n.us1.codeanyapp.com',
+    'lottonero-e7dc9a7038d2.herokuapp.com',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-jjsemaan-lottonero-jwr574l49n.us1.codeanyapp.com',
+    'lottonero-e7dc9a7038d2.herokuapp.com',
 ]
 
 # Application definition
