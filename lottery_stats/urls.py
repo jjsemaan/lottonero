6c4791bd1834e084b59577cb89dc5d0c3c98307e
jjsemaan <jalal.semaan@parabim.com>
2024-05-06 14:02:43 +0000

    Split stats view to three separate views

--- lottery_stats/urls.py
+++ lottery_stats/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import stats_view
+from . import views
 
 urlpatterns = [
-    path('', stats_view, name='stats_view'),
+    path('frequencies/', views.frequency_view, name='frequencies'),
+    path('combinations/', views.combinations_view, name='combinations'),
+    path('correlations/', views.correlations_view, name='correlations'),
 ]
