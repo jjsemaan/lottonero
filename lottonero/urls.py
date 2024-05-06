5c4f2d5f2d2ed62fb2f77606ff5a863e0e33852c
jjsemaan <jalal.semaan@parabim.com>
2024-05-06 07:23:34 +0000

    Add statistics page and initial view

--- lottonero/urls.py
+++ lottonero/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('predictions/', include('predictions.urls')),
+    path('lottery_stats/', include('lottery_stats.urls')),
     path('scraping/', include(('scraping.urls', 'scraping'), namespace='scraping')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
