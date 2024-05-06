73a71356ea2c1a5f271f485b28367ae708520107
jjsemaan <jalal.semaan@parabim.com>
2024-05-06 15:46:54 +0000

    Modify secret key in settings

--- lottonero/settings.py
+++ lottonero/settings.py
@@ -17,20 +17,28 @@ SECRET_KEY = os.environment.get("SECRET_KEY")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-jjsemaan-lottonero-jwr574l49n.us1.codeanyapp.com',
-    'lottonero-e7dc9a7038d2.herokuapp.com',
+    'lottonero-e7dc9a7038d2.herokuapp.com'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'https://8000-jjsemaan-lottonero-jwr574l49n.us1.codeanyapp.com',
+    'https://lottonero-e7dc9a7038d2.herokuapp.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-jjsemaan-lottonero-jwr574l49n.us1.codeanyapp.com',
+    'https://lottonero-e7dc9a7038d2.herokuapp.com'
 ]
 
 # Application definition
