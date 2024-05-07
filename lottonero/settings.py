a57509dfb7b5ee6b99970f3ef49917e70d719333
jjsemaan <jalal.semaan@parabim.com>
2024-05-07 18:37:58 +0000

    Resolve branching error

--- lottonero/settings.py
+++ lottonero/settings.py
@@ -14,10 +14,6 @@ DATABASES = {
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
@@ -174,11 +170,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-
-import os
-
-# Your BASE_DIR definition goes here
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
