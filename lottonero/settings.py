dcfca5c9758e067d559f2ccd4ee6e0dc95334be2
jjsemaan <jalal.semaan@parabim.com>
2024-05-07 15:10:07 +0000

    Remove trailing slash in ststic root

--- lottonero/settings.py
+++ lottonero/settings.py
@@ -172,14 +172,18 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+import os
+
+# Your BASE_DIR definition goes here
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Remove the trailing slash
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Default primary key field type
