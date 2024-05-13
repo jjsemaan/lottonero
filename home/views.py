6e6a8e4ecc8e157f480b1ba406d6771322c376ec
jjsemaan <jalal.semaan@parabim.com>
2024-05-13 20:49:59 +0000

    Add prize checking model and button to backoffice

--- home/views.py
+++ home/views.py
@@ -30,7 +30,7 @@ def latest_predictions_with_matches(request):
         latest_result = EuroMillionsResult.objects.latest('id')
         latest_date = Prediction.objects.aggregate(latest_date=Max('prediction_date'))['latest_date']
         latest_predictions = Prediction.objects.filter(prediction_date=latest_date)
-        
+
         matches = []
         for prediction in latest_predictions:
             prediction_numbers = {
@@ -41,13 +41,37 @@ def latest_predictions_with_matches(request):
                 latest_result.ball_1, latest_result.ball_2, latest_result.ball_3,
                 latest_result.ball_4, latest_result.ball_5
             }
+            prediction_lucky_numbers = {prediction.pred_lucky_1, prediction.pred_lucky_2}
+            draw_lucky_numbers = {latest_result.lucky_star_1, latest_result.lucky_star_2}
 
             common_numbers = prediction_numbers & draw_numbers
-            if len(common_numbers) == 2:
-                match_info = {
-                    'prediction_id': prediction.id,
-                    'match_info': 'Match 2'
-                }
+            common_lucky_numbers = prediction_lucky_numbers & draw_lucky_numbers
+            num_common_numbers = len(common_numbers)
+            num_common_lucky_numbers = len(common_lucky_numbers)
+
+            match_info = {
+                'prediction_id': prediction.id,
+                'match_info': None
+            }
+
+            if num_common_numbers == 5 and num_common_lucky_numbers == 2:
+                match_info['match_info'] = 'Match 5 + 2'
+            elif num_common_numbers == 5:
+                match_info['match_info'] = 'Match 5'
+            elif num_common_numbers == 4 and num_common_lucky_numbers == 1:
+                match_info['match_info'] = 'Match 4 + 1'
+            elif num_common_numbers == 4:
+                match_info['match_info'] = 'Match 4'
+            elif num_common_numbers == 3 and num_common_lucky_numbers == 1:
+                match_info['match_info'] = 'Match 3 + 1'
+            elif num_common_numbers == 3:
+                match_info['match_info'] = 'Match 3'
+            elif num_common_numbers == 2 and num_common_lucky_numbers == 1:
+                match_info['match_info'] = 'Match 2 + 1'
+            elif num_common_numbers == 2:
+                match_info['match_info'] = 'Match 2'
+
+            if match_info['match_info']:
                 matches.append(match_info)
                 print(f"Match found: {match_info}")  # Output to the terminal
 
