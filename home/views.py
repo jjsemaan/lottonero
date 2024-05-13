3b5bbd9791060faf6296e8f90f70164a4cf9f04b
jjsemaan <jalal.semaan@parabim.com>
2024-05-13 20:28:57 +0000

    Add view to check match 2 in predicted balls

--- home/views.py
+++ home/views.py
@@ -16,3 +16,41 @@ def latest_draw(request):
         'latest_result': latest_result,
     }
     return render(request, 'home/index.html', context)
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from scraping.models import EuroMillionsResult
+from predictions.models import Prediction
+from django.db.models import Max
+
+def latest_predictions_with_matches(request):
+    if request.method == 'POST':
+        latest_result = EuroMillionsResult.objects.latest('id')
+        latest_date = Prediction.objects.aggregate(latest_date=Max('prediction_date'))['latest_date']
+        latest_predictions = Prediction.objects.filter(prediction_date=latest_date)
+        
+        matches = []
+        for prediction in latest_predictions:
+            prediction_numbers = {
+                prediction.pred_ball_1, prediction.pred_ball_2, prediction.pred_ball_3,
+                prediction.pred_ball_4, prediction.pred_ball_5
+            }
+            draw_numbers = {
+                latest_result.ball_1, latest_result.ball_2, latest_result.ball_3,
+                latest_result.ball_4, latest_result.ball_5
+            }
+
+            common_numbers = prediction_numbers & draw_numbers
+            if len(common_numbers) == 2:
+                match_info = {
+                    'prediction_id': prediction.id,
+                    'match_info': 'Match 2'
+                }
+                matches.append(match_info)
+                print(f"Match found: {match_info}")  # Output to the terminal
+
+        return HttpResponse("Check terminal for match results.")
+    else:
+        return HttpResponse("This endpoint only accepts POST requests.")
--- predictions/urls.py
+++ predictions/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import train_classifier
 from .views import display_predictions
+from home.views import latest_predictions_with_matches
 from . import views
 
 urlpatterns = [
     path('predictions/', display_predictions, name='predictions'),
     path('backoffice/', views.backoffice, name='backoffice'),
     path('train/', train_classifier, name='train_classifier'),
+    path('check-winning-predictions/', latest_predictions_with_matches, name='latest_predictions_with_matches'),
 ]
