ad869f135e77cd0f1dadfdf51a6ea48cedd5b8f7
jjsemaan <jalal.semaan@parabim.com>
2024-05-25 10:42:58 +0000

    Convert winning numbers to balls in latest prediction table

---
+++ home/templatetags/custom_filters.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def split_and_strip(value, delimiter=","):
+    return [item.strip() for item in value.split(delimiter)]
+
+@register.filter
+def to_int(value):
+    try:
+        return int(value) if value else None
+    except (ValueError, TypeError):
+        return None
