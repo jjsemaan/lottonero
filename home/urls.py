7b51b5f4c8105b636b77e6c55af5c0fa1a9f0f6a
jjsemaan <jalal.semaan@parabim.com>
2024-05-25 01:13:37 +0000

    Failed to retrieve all winning history from predictions table

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from .views import index, latest_predictions_with_matches
 
 urlpatterns = [
     path('', index, name='home'),  # Homepage pointing to index
-    path('latest-predictions-with-matches/', latest_predictions_with_matches, name='latest_predictions_with_matches'),  # Endpoint for updating matches
+    path('latest-predictions-with-matches/', latest_predictions_with_matches, name='latest_predictions_with_matches'),
 ]
