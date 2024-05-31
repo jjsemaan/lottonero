9e8481c4bd2c8ff6f812dce5490083017653fd38
jjsemaan <jalal.semaan@parabim.com>
2024-05-31 18:38:33 +0000

    Add new model for shuffled predictions

--- predictions/models.py
+++ predictions/models.py
@@ -22,7 +22,21 @@ class Prediction(models.Model):
     def __str__(self):
         return f"Prediction on {self.prediction_date} - {self.match_type if self.match_type else 'No match'}"
 
+class ShuffledPrediction(models.Model):
+    prediction_date = models.CharField(max_length=10)  # YYYY/MM/DD format
+    draw_date = models.CharField(max_length=10, null=True, blank=True)  # New field for draw date
+    pred_ball_1 = models.IntegerField()
+    pred_ball_2 = models.IntegerField()
+    pred_ball_3 = models.IntegerField()
+    pred_ball_4 = models.IntegerField()
+    pred_ball_5 = models.IntegerField()
+    pred_lucky_1 = models.IntegerField()
+    pred_lucky_2 = models.IntegerField()
+    
+    def __str__(self):
+        return f"Shuffled Prediction on {self.prediction_date}"
 
+        
 from cloudinary.models import CloudinaryField
 
 class UploadImageModel(models.Model):
