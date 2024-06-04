de4e50ef69845cfb861b277b2466a1e3890c8cf4
jjsemaan <jalal.semaan@parabim.com>
2024-06-04 19:27:26 +0000

    Add Stripe Elements js

--- orders/views.py
+++ orders/views.py
@@ -78,3 +78,25 @@ def subscription_success(request):
 
 def subscription_success(request):
     return render(request, 'checkout/subscription_success.html')
+
+"""
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+        'client_secret': 'test client secret',
+    }
+
+    return render(request, template, context)
+"""
