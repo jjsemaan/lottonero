dc41e164742cf3028f33075fd35081a52bd8ed1d
jjsemaan <jalal.semaan@parabim.com>
2024-06-09 15:37:01 +0000

    Add styles to combination buttons in index

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', index, name='home'),
     path('latest-predictions-with-matches/', latest_predictions_with_matches, name='latest_predictions_with_matches'),
     path('alltime/', alltime_winning_predictions_view, name='alltime_predictions'),
-    path('latest-shuffled-predictions-with-matches/', latest_shuffled_predictions_with_matches, name='latest_shuffled_predictions_with_matches'),
+    path('latest-shuffled-predictions-with-matches/', latest_shuffled_predictions_with_matches, name='latest_combination_predictions_with_matches'),
 ]
