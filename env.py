74aa727203df99919fb77368a37a814d98ec4973
Jalal Jsemaan <jalal.semaan@parabim.com>
2024-06-17 21:54:21 +0000

    Update webhook secret

--- env.py
+++ env.py
@@ -19,7 +19,7 @@ os.environ['DEFAULT_FROM_EMAIL'] = 'donotreply@lottonero.com'
 
 os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51PNZDORqKPcp1RrsbRAW3U80hXCOvyDMKYGEqQYRPtIHPsnECtRdfGUqdfbPXvCRNUzd2p9JOTktuqBC06HbBXeI00ZFkWdNvq'
 os.environ['STRIPE_PUBLISHABLE_KEY'] = 'pk_test_51PNZDORqKPcp1Rrs1PYOOcfQ64miVJRpB2Iyyg8zse2ZK7h7rmqcqT6Wb6rIFUJhfv9QGcXYuj42eRA2kkLekmb200zB1Rq0N5'
-os.environ['STRIPE_ENDPOINT_SECRET'] = 'whsec_ypKEFXNzBNfHFAgsQdrwkGGkOGnr3Z9t'
+os.environ['STRIPE_ENDPOINT_SECRET'] = 'whsec_SReID4QovD8y1pBU2MxuGIy6s983oEOm'
 os.environ['STRIPE_PRICING_TABLE_ID'] ='prctbl_1PRy3kRqKPcp1RrsSNlyexrM'
 os.environ['DJSTRIPE_WEBHOOK_SECRET'] = 'your_stripe_webhook_secret' # not recommended and will be removed in the future
 
