13756098542bd8a4a24c014da39e769573e71d97
Jalal Jsemaan <jalal.semaan@parabim.com>
2024-06-18 13:14:52 +0000

    Update pricing table id

--- env.py
+++ env.py
@@ -20,7 +20,7 @@ os.environ['DEFAULT_FROM_EMAIL'] = 'donotreply@lottonero.com'
 os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51PNZDORqKPcp1RrsbRAW3U80hXCOvyDMKYGEqQYRPtIHPsnECtRdfGUqdfbPXvCRNUzd2p9JOTktuqBC06HbBXeI00ZFkWdNvq'
 os.environ['STRIPE_PUBLISHABLE_KEY'] = 'pk_test_51PNZDORqKPcp1Rrs1PYOOcfQ64miVJRpB2Iyyg8zse2ZK7h7rmqcqT6Wb6rIFUJhfv9QGcXYuj42eRA2kkLekmb200zB1Rq0N5'
 os.environ['STRIPE_ENDPOINT_SECRET'] = 'whsec_fK05NWA8TjRPl2D8Lij4iP5qfYGX9If8'
-os.environ['STRIPE_PRICING_TABLE_ID'] = 'prctbl_1PRy3kRqKPcp1RrsSNlyexrM'
+os.environ['STRIPE_PRICING_TABLE_ID'] = 'prctbl_1PT1omRqKPcp1RrsqJDYG681'
 os.environ['DJSTRIPE_WEBHOOK_SECRET'] = 'whsec_fK05NWA8TjRPl2D8Lij4iP5qfYGX9If8' # not recommended and will be removed in the future
 
 os.environ['CLOUDINARY_CLOUD_NAME'] = 'dmebot5lf'
