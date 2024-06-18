f34da040f35f5fa502e627812f11e10fe9762195
Jalal Jsemaan <jalal.semaan@parabim.com>
2024-06-18 12:57:01 +0000

    Add new webhook secret

--- env.py
+++ env.py
@@ -19,9 +19,9 @@ os.environ['DEFAULT_FROM_EMAIL'] = 'donotreply@lottonero.com'
 
 os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51PNZDORqKPcp1RrsbRAW3U80hXCOvyDMKYGEqQYRPtIHPsnECtRdfGUqdfbPXvCRNUzd2p9JOTktuqBC06HbBXeI00ZFkWdNvq'
 os.environ['STRIPE_PUBLISHABLE_KEY'] = 'pk_test_51PNZDORqKPcp1Rrs1PYOOcfQ64miVJRpB2Iyyg8zse2ZK7h7rmqcqT6Wb6rIFUJhfv9QGcXYuj42eRA2kkLekmb200zB1Rq0N5'
-os.environ['STRIPE_ENDPOINT_SECRET'] = 'whsec_xjCAASOTIQt5aReI3UPk3tK6uMfbeXKb'
+os.environ['STRIPE_ENDPOINT_SECRET'] = 'whsec_fK05NWA8TjRPl2D8Lij4iP5qfYGX9If8'
 os.environ['STRIPE_PRICING_TABLE_ID'] = 'prctbl_1PRy3kRqKPcp1RrsSNlyexrM'
-os.environ['DJSTRIPE_WEBHOOK_SECRET'] = 'whsec_xjCAASOTIQt5aReI3UPk3tK6uMfbeXKb' # not recommended and will be removed in the future
+os.environ['DJSTRIPE_WEBHOOK_SECRET'] = 'whsec_fK05NWA8TjRPl2D8Lij4iP5qfYGX9If8' # not recommended and will be removed in the future
 
 os.environ['CLOUDINARY_CLOUD_NAME'] = 'dmebot5lf'
 os.environ['CLOUDINARY_API_KEY'] = '765648534614641'
