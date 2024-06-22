819574f24d5ab12293563e293d8b9086eea4b3ee
Jalal Jsemaan <jalal.semaan@parabim.com>
2024-06-22 10:42:14 +0000

    Add username to profile.html page

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from allauth.account.models import EmailAddress
 from django.contrib import messages
@@ -8,33 +9,39 @@ from orders.models import Subscription
 @login_required
 def profile_view(request):
     user = request.user
-    email = EmailAddress.objects.get(user=user)
+    primary_email = EmailAddress.objects.filter(user=user, primary=True).first()  # Retrieve the primary email
     subscriptions = Subscription.objects.filter(user=user)
     return render(request, 'user_profile/profile.html', {
         'user': user,
-        'email': email,
+        'primary_email': primary_email,  # Pass the primary email to the template
         'subscriptions': subscriptions
     })
-
 
 @login_required
 def update_profile(request):
     user = request.user
-    email = EmailAddress.objects.get(user=user)
+    primary_email = EmailAddress.objects.filter(user=user, primary=True).first()
     subscriptions = Subscription.objects.filter(user=user)  # Fetch subscriptions for both POST and GET
 
     if request.method == 'POST':
         user.first_name = request.POST.get('first_name', user.first_name)
         user.last_name = request.POST.get('last_name', user.last_name)
+
+        # Update username if your policy allows it
+        username = request.POST.get('username', user.username)
+        user.username = username  # Make sure to handle unique constraint
+
         user.save()
 
-        
         messages.success(request, 'Your profile was updated successfully.')
-        return render(request, 'user_profile/profile.html', {
-            'user': user,
-            'email': email,
-            'subscriptions': subscriptions
-        })
+        render(request, 'user_profile/profile.html')  # Redirect to a profile view page to avoid double submission
+
+    # Display the profile edit page
+    return render(request, 'user_profile/profile.html', {
+        'user': user,
+        'primary_email': primary_email,
+        'subscriptions': subscriptions
+    })
 
 @login_required
 def change_password(request):
