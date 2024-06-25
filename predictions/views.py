db2b5c2ece083d8726d6f8974c044ce6d2608bca
Jalal Jsemaan <jalal.semaan@parabim.com>
2024-06-25 21:41:39 +0000

    Resolved train clasifier datetime bug by removing validation

--- predictions/views.py
+++ predictions/views.py
@@ -138,192 +138,95 @@ def backoffice(request):
     return render(request, "backoffice/backoffice.html")
 
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-import numpy as np
-import pandas as pd
 from django.views.decorators.csrf import csrf_protect
 from django.utils import timezone
-from django.http import HttpResponseBadRequest, HttpResponseServerError
-import datetime
-from scraping.models import EuroMillionsResult
-
+from django.http import HttpResponseBadRequest, HttpResponseServerError, HttpResponse
+from django.shortcuts import render
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from django.contrib.admin.views.decorators import staff_member_required
 
 @admin_required
 @csrf_protect
 def train_classifier(request):
-    """
-    Handle POST requests to train a classifier and generate lottery
-    predictions.
-
-    This view function processes a POST request to train a
-    RandomForestClassifier using historical EuroMillions lottery results and
-    generates unique lottery number predictions. The predictions are stored in
-    the database and appropriate templates are rendered based on the result.
-
-    Parameters:
-    request (HttpRequest): The HTTP request object containing a POST request
-    with a 'draw_date' field.
-
-    Returns:
-    HttpResponse: Renders a template with a success message and new predictions
-                  if successful.
-                  Renders a template with a message if predictions for the
-                  draw date already exist.
-                  Returns a 400 Bad Request response if the 'draw_date'
-                  is missing or invalid.
-                  Returns a 500 Internal Server Error response if any error
-                  occurs during processing.
-
-    The function performs the following steps:
-    1. Validates the presence and format of 'draw_date' in the POST request.
-    2. Checks if predictions for the provided draw date already exist.
-    3. If no existing predictions are found:
-       a. Retrieves historical EuroMillions results from the database.
-       b. Trains two RandomForestClassifier models: one for predicting main
-          balls and another for lucky stars.
-       c. Generates unique predictions ensuring no duplicate sets of numbers.
-       d. Saves the new predictions to the database.
-       e. Renders a template displaying the new predictions.
-    4. Handles errors by returning appropriate HTTP responses.
-    """
-
-    if request.method == "POST":
-        draw_date = request.POST.get("draw_date")
-
-        if not draw_date:
-            return HttpResponseBadRequest("Draw date is required.")
-
-        # Convert the draw_date to YYYY/MM/DD format
-        try:
-            draw_date_obj = datetime.datetime.strptime(draw_date, "%Y-%m-%d")
-            draw_date_str = draw_date_obj.strftime("%Y/%m/%d")
-        except ValueError:
-            return HttpResponseBadRequest("Invalid date format.")
-
-        # Check if predictions for this draw date already exist
-        if Prediction.objects.filter(draw_date=draw_date_str).exists():
-            return render(
-                request,
-                "backoffice/predictions_exist.html",
-                {"message": "Predictions for this draw date already exist."},
-            )
-
-        try:
-            # Verify the last scraped draw_date
-            last_scraped_result = EuroMillionsResult.objects.order_by(
-                "-draw_date"
-            ).first()
-            today_str = timezone.now().strftime("%Y/%m/%d")
-
-            if not last_scraped_result:
-                return render(
-                    request,
-                    "backoffice/scrape_first.html",
-                    {"message": "Please scrape the latest results first."},
-                )
-
-            if isinstance(last_scraped_result.draw_date, str):
-                last_scraped_date = datetime.datetime.strptime(
-                    last_scraped_result.draw_date, "%Y/%m/%d"
-                )
-            else:
-                last_scraped_date = last_scraped_result.draw_date
-            # Strictly predict on same day as scraped
-            if last_scraped_date.strftime("%Y/%m/%d") != today_str:
-                return render(
-                    request,
-                    "backoffice/scrape_first.html",
-                    {"message": "Please scrape the latest results first."},
-                )
-
-            data = EuroMillionsResult.objects.values(
-                "ball_1",
-                "ball_2",
-                "ball_3",
-                "ball_4",
-                "ball_5",
-                "lucky_star_1",
-                "lucky_star_2",
-            )
-            df = pd.DataFrame(list(data))
-
-            X = df.drop(["lucky_star_1", "lucky_star_2"], axis=1)
-            y_balls = df[["ball_1", "ball_2", "ball_3", "ball_4", "ball_5"]]
-            y_lucky = df[["lucky_star_1", "lucky_star_2"]]
-
-            X_arr = X.values
-            y_balls_arr = y_balls.values
-            y_lucky_arr = y_lucky.values
-
-            X_train_balls, X_test_balls, y_train_balls, y_test_balls = (
-                train_test_split(
-                    X_arr, y_balls_arr, test_size=0.3, random_state=42
-                )
-            )
-            X_train_lucky, X_test_lucky, y_train_lucky, y_test_lucky = (
-                train_test_split(
-                    X_arr, y_lucky_arr, test_size=0.3, random_state=42
-                )
-            )
-
-            rf_classifier_balls = RandomForestClassifier(
-                n_estimators=100, random_state=42
-            )
-            rf_classifier_lucky = RandomForestClassifier(
-                n_estimators=100, random_state=42
-            )
-            rf_classifier_balls.fit(X_train_balls, y_train_balls)
-            rf_classifier_lucky.fit(X_train_lucky, y_train_lucky)
-
-            unique_balls_sets = set()
-            unique_full_sets = set()
-            predictions = []
-
-            # Generate predictions ensuring all are unique
-            for _ in range(100):  # Limit to a reasonable number of attempts
-                if len(predictions) >= len(X_test_balls):
-                    break
-                y_pred_balls = rf_classifier_balls.predict(X_test_balls)
-                y_pred_lucky = rf_classifier_lucky.predict(X_test_lucky)
-
-                for ball_pred, lucky_pred in zip(y_pred_balls, y_pred_lucky):
-                    if len(set(ball_pred)) == 5 and len(set(lucky_pred)) == 2:
-                        ball_set = tuple(sorted(ball_pred))
-                        lucky_set = tuple(sorted(lucky_pred))
-                        full_set = ball_set + lucky_set
-
-                        if (
-                            ball_set not in unique_balls_sets
-                            and full_set not in unique_full_sets
-                        ):
-                            unique_balls_sets.add(ball_set)
-                            unique_full_sets.add(full_set)
-                            prediction = Prediction(
-                                prediction_date=today_str,
-                                draw_date=draw_date_str,
-                                pred_ball_1=ball_pred[0],
-                                pred_ball_2=ball_pred[1],
-                                pred_ball_3=ball_pred[2],
-                                pred_ball_4=ball_pred[3],
-                                pred_ball_5=ball_pred[4],
-                                pred_lucky_1=lucky_pred[0],
-                                pred_lucky_2=lucky_pred[1],
-                            )
-                            prediction.save()
-                            predictions.append(prediction)
-
-            return render(
-                request,
-                "backoffice/new_predictions.html",
-                {"message": "New predictions added to the database."},
-            )
-
-        except Exception as e:
-            return HttpResponseServerError(f"An error occurred: {e}")
-
-    else:
+    if request.method != "POST":
         return render(request, "backoffice/backoffice.html")
+
+    draw_date = request.POST.get("draw_date")
+    if not draw_date:
+        return HttpResponseBadRequest("Draw date is required.")
+
+    draw_date_str = draw_date.replace('-', '/')  # Ensure the format is YYYY/MM/DD by replacing hyphens with slashes
+
+    # Check if predictions for this draw date already exist
+    if Prediction.objects.filter(draw_date=draw_date_str).exists():
+        return render(request, "backoffice/predictions_exist.html", {"message": "Predictions for this draw date already exist."})
+
+    try:
+        # Get the most recent scrape's date and ensure it matches today's date
+        last_scraped_result = EuroMillionsResult.objects.order_by("-draw_date").first()
+        if not last_scraped_result:
+            return render(request, "backoffice/scrape_first.html", {"message": "Please scrape the latest results first."})
+
+        last_scraped_date_str = last_scraped_result.draw_date
+        today_str = timezone.now().strftime("%Y/%m/%d")
+        if last_scraped_date_str != today_str:
+            return render(request, "backoffice/scrape_first.html", {"message": "Please scrape the latest results first."})
+
+        # Load and prepare the dataset
+        data = EuroMillionsResult.objects.values("ball_1", "ball_2", "ball_3", "ball_4", "ball_5", "lucky_star_1", "lucky_star_2")
+        df = pd.DataFrame(list(data))
+        X = df.drop(["lucky_star_1", "lucky_star_2"], axis=1)
+        y_balls = df[["ball_1", "ball_2", "ball_3", "ball_4", "ball_5"]]
+        y_lucky = df[["lucky_star_1", "lucky_star_2"]]
+
+        # Train classifiers
+        X_train_balls, X_test_balls, y_train_balls, y_test_balls = train_test_split(X, y_balls, test_size=0.3, random_state=42)
+        X_train_lucky, X_test_lucky, y_train_lucky, y_test_lucky = train_test_split(X, y_lucky, test_size=0.3, random_state=42)
+        rf_classifier_balls = RandomForestClassifier(n_estimators=100, random_state=42)
+        rf_classifier_lucky = RandomForestClassifier(n_estimators=100, random_state=42)
+        rf_classifier_balls.fit(X_train_balls, y_train_balls)
+        rf_classifier_lucky.fit(X_train_lucky, y_train_lucky)
+
+        unique_balls_sets = set()
+        unique_full_sets = set()
+        predictions = []
+
+        # Generate predictions ensuring all are unique
+        for _ in range(100):  # Limit to a reasonable number of attempts
+            y_pred_balls = rf_classifier_balls.predict(X_test_balls)
+            y_pred_lucky = rf_classifier_lucky.predict(X_test_lucky)
+
+            for ball_pred, lucky_pred in zip(y_pred_balls, y_pred_lucky):
+                if len(set(ball_pred)) == 5 and len(set(lucky_pred)) == 2:
+                    ball_set = tuple(sorted(ball_pred))
+                    lucky_set = tuple(sorted(lucky_pred))
+                    full_set = ball_set + lucky_set
+
+                    if ball_set not in unique_balls_sets and full_set not in unique_full_sets:
+                        unique_balls_sets.add(ball_set)
+                        unique_full_sets.add(full_set)
+                        prediction = Prediction(
+                            prediction_date=today_str,
+                            draw_date=draw_date_str,
+                            pred_ball_1=ball_pred[0],
+                            pred_ball_2=ball_pred[1],
+                            pred_ball_3=ball_pred[2],
+                            pred_ball_4=ball_pred[3],
+                            pred_ball_5=ball_pred[4],
+                            pred_lucky_1=lucky_pred[0],
+                            pred_lucky_2=lucky_pred[1],
+                        )
+                        prediction.save()
+                        predictions.append(prediction)
+
+        return render(request, "backoffice/new_predictions.html", {"message": "New predictions added to the database."})
+
+    except Exception as e:
+        return HttpResponseServerError(f"An error occurred: {e}")
+
+
 
 
 @admin_required
